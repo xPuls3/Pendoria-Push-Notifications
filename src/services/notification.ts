70cb44d7697cae57d50aab10db25e48f2d62a494
xPuls3 <Xer0.Puls3.XXII@gmail.com>
2021-05-08 18:51:28 -0400

    Removed a console log

--- src/services/notification.ts
+++ src/services/notification.ts
@@ -17,8 +17,6 @@ export default class NotificationService {
 
     public static async notify (title: string, text: string): Promise<void> {
 
-        console.log("Sending notification", ...arguments);
-
         const hasPermission = await NotificationService.hasPermission();
         if (hasPermission === false) return;
 
